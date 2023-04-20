10079657aaeb077b6863c2e6f3fa81af18a831ce
khadija ED-DRAIJI <khadija.eddraiji@gmail.com>
2023-04-20 16:49:26 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - print a c program using printf
  *
- * Return: 0
+ * Return: 0 
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art.\n");
 	return (0);
